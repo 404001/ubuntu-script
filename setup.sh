08e5d517d3839f00ca9d87e35228db502729a691
404001 <gaviloelor@gmail.com>
2024-10-08 00:58:49 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -36,4 +36,12 @@ EOL
 # Reboot to apply changes
 echo "The system will reboot in 5 seconds to apply changes..."
 sleep 5
+echo "4"
+sleep 4
+echo "3"
+sleep 3
+echo "2"
+sleep 2
+echo "1"
+sleep 1
 sudo reboot
