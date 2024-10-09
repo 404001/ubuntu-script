faf779b999eecac5056d3408f0391c4bcb065b17
404001 <gaviloelor@gmail.com>
2024-10-09 19:41:57 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
+# Actualización e instalación de paquetes
 sudo apt update && sudo apt upgrade -y
 
-sudo apt install -y git curl wget ipcalc htop kitty gnome-tweaks gnome-shell-extensions dconf-cli neofetch
+sudo apt install -y git curl wget ipcalc htop kitty gnome-tweaks gnome-shell-extensions dconf-cli neofetch gnome-shell-extension-tool unzip
 
+# Cambiar el emulador de terminal predeterminado a kitty
 sudo update-alternatives --set x-terminal-emulator /usr/bin/kitty
 
+# Configuración de GNOME
 gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
-
 gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
 gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 32
-
 gsettings set org.gnome.desktop.background show-desktop-icons false
 
-
+# Descargar e instalar fondo de pantalla
 WALLPAPER_URL="https://github.com/404001/ubuntu-script/blob/main/background.png?raw=true"
 wget -O ~/background.png $WALLPAPER_URL
 gsettings set org.gnome.desktop.background picture-uri "file:///$HOME/background.png"
 
+# Configuración de Kitty
 KITTY_CONFIG_DIR="$HOME/.config/kitty"
 mkdir -p $KITTY_CONFIG_DIR
 
@@ -32,8 +34,22 @@ EOL
 
 kitty +kitten themes wez
 
-# Reboot to apply changes
-echo "The system will reboot in 5 seconds to apply changes..."
+# Descargar e instalar la extensión LAN IP Address
+EXTENSION_URL="https://extensions.gnome.org/extension-data/lanipaddressuNmIND.zip"
+EXTENSION_DIR="$HOME/.local/share/gnome-shell/extensions"
+
+mkdir -p $EXTENSION_DIR
+wget -O /tmp/lan-ip-address.zip $EXTENSION_URL
+unzip /tmp/lan-ip-address.zip -d $EXTENSION_DIR/lan-ip-address@uNmIND
+
+# Habilitar la extensión
+gnome-extensions enable lan-ip-address@uNmIND
+
+# Recargar GNOME Shell (solo funciona si usas X11)
+gnome-shell --replace &
+
+# Reiniciar para aplicar los cambios (si es necesario)
+echo "El sistema se reiniciará en 5 segundos para aplicar los cambios..."
 sleep 5
 echo "4"
 sleep 4
